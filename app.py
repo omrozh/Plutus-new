dd363c9537cdc2af01981790149c62a3ab8dc347
omrozh <53612475+omrozh@users.noreply.github.com>
2023-11-20 12:16:31 +0300

    safe close

--- app.py
+++ app.py
@@ -144,10 +144,11 @@ def check_and_update_code():
 
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=check_and_update_code, trigger="interval", seconds=30)
 
 for i in scheduler.get_jobs():
     scheduler.remove_job(i.id)
+
+scheduler.add_job(func=check_and_update_code, trigger="interval", seconds=30)
 
 scheduler.start()
 
